f33f80bcd7fb55aacd1d95ae69dc21132c564cba
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-08-19 16:33:37 -0600

    Pract 01: Fix visual bugs
    Features:
    - Función para eliminar widgets previamente mostrados
    - Resolución default más grande para mostrar mejor las imágenes
    - Menor bug fix para guardar imágenes s

--- 01_gui/main.py
+++ 01_gui/main.py
@@ -8,8 +8,8 @@ from PIL import Image, ImageOps, ImageTk
 ## Inicialización del gestor de ventana
 root = tk.Tk()
 root.title("Imagen a Escala de Grises")
-root.geometry("640x480")
-root.minsize(320, 240)
+root.geometry("924x520")
+root.minsize(640, 480)
 
 ## Configuración de como las filas y columnas se van a distribuir sobre la ventana
 root.grid_columnconfigure((0, 1), weight=1)
@@ -27,10 +27,15 @@ for row in range(num_rows):
         frame.grid(row=row, column=column, pady=5, sticky="nsew")
         frames.append(frame)
 
+## Definición de utilidades para crear y destruir widgets 
 def add_button(frame: tk.Frame, text: str, command: Callable) -> tk.Button:
     button = tk.Button(frame, text=text, width=20, command=command)
     button.pack(side="top")
     return button
+
+def delete_children_widgets(frame: tk.Frame) -> None:
+    for widget in frame.winfo_children():
+        widget.destroy()
 
 ## Botón para cargar/seleccionar una imagen
 file_path: Path | str | None = None
@@ -38,10 +43,12 @@ gray_image: Image.Image | None = None
 
 def open_image() -> None:
     global file_path
+    global gray_image
+
     file_path = filedialog.askopenfilename(
         title = "Seleccione una imagen para convertirla a escala de grises",
         initialdir = "~/downloads",
-        filetypes = [("Imágenes", ["*.png", "*.jpg", "*.jpeg", "*.svg"])]
+        filetypes = [("Imágenes", ["*.png", "*.jpg", "*.jpeg"])]
     )
     
     ## Abrir y transformar la imagen en el formato que tkinter espera
@@ -50,13 +57,15 @@ def open_image() -> None:
         file_path = Path(file_path)
         color_image = Image.open(file_path)
         color_image = ImageOps.exif_transpose(color_image) # Preservar la orientación original de la imagen usando los metadatos
-        
-        global gray_image
-        gray_image = color_image.convert("L")
-        
-        insert_image(color_image, frames[2])
 
-        add_button(frames[1], "Guardar", save_image)
+        ## Eliminar widgets o botones mostrados previamente
+        if gray_image:
+            list(map(delete_children_widgets, frames[1:]))
+
+        insert_image(color_image, frames[2])
+        
+        button_save = add_button(frames[1], "Guardar", save_image)
+        gray_image = color_image.convert("L")
         insert_image(gray_image, frames[3])
 
 def insert_image(pil_image: Image.Image, frame: tk.Frame) -> None:
@@ -84,9 +93,10 @@ def save_image() -> None:
             defaultextension = extension,
             filetypes = [(f"Imagen {extension.upper()}", f"*.{extension}")]
         )
-        save_file_path = Path(save_file_path)
-
-        gray_image.save(save_file_path)
+        
+        if save_file_path:
+            save_file_path = Path(save_file_path)
+            gray_image.save(save_file_path)
 
 if __name__ == "__main__":
     root.mainloop()
