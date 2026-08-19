3a6c28bb976a62a72843449f2b86bfb7cfb07176
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-08-19 15:02:47 -0600

    Pract 01: Improves at GUI
    Features:
    - Las imágenes se muestran centradas al widget
    - Las imágenes al ser cargadas usan la misma orientación que se muestra en sus metadatos
    - Mejoras menores a la estética de la GUI

--- 01_gui/main.py
+++ 01_gui/main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 from pathlib import Path
 
-from PIL import Image, ImageTk
+from PIL import Image, ImageOps, ImageTk
 
 ## Inicialización del gestor de ventana
 root = tk.Tk()
@@ -19,11 +19,10 @@ root.grid_rowconfigure(1, weight=1)
 num_columns = 2
 num_rows = 2
 frames: list[tk.Frame] = []
-colors = [["red", "blue"], ["green", "yellow"]]
 
 for row in range(num_rows):
     for column in range(num_columns):
-        frame = tk.Frame(root, width=75, height=50, bg=colors[row][column])
+        frame = tk.Frame(root, width=75, height=50)
         frame.grid(row=row, column=column, pady=5, sticky="nsew")
         frames.append(frame)
 
@@ -44,6 +43,7 @@ def open_image() -> None:
     if file_path:
         file_path = Path(file_path)
         color_image = Image.open(file_path)
+        color_image = ImageOps.exif_transpose(color_image) # Preservar la orientación original de la imagen usando los metadatos
         
         global gray_image
         gray_image = color_image.convert("L")
@@ -52,16 +52,15 @@ def open_image() -> None:
         insert_image(gray_image, frames[3])
 
 def insert_image(pil_image: Image.Image, frame: tk.Frame) -> None:
-    aspect_ratio = int.__truediv__(*pil_image.size)
-
-    frm_width = frame.winfo_width()*0.5
-    fmr_height = frame.winfo_height()*0.5
+    ## Reescalamiento de la imagen para que ocupe todo el widget
+    frm_width = frame.winfo_width()
+    fmr_height = frame.winfo_height()
     image = pil_image.copy()
-    image.thumbnail((int(aspect_ratio*frm_width),int(aspect_ratio*fmr_height)))
+    image.thumbnail((frm_width,fmr_height))
 
     image = ImageTk.PhotoImage(image)
     image_label = tk.Label(frame, image=image, justify="center")
-    image_label.grid(column=1, row=1)
+    image_label.pack(side="top")
     image_label.image = image # Referencia adicional para evitar que el garbage collector elimine la imagen antes de mostrarla en la GUI
 
 button_open = tk.Button(frames[0], text="Abrir", width=20, command=open_image)
@@ -74,9 +73,8 @@ def save_image() -> None:
         save_file_path = filedialog.asksaveasfilename(
             title = "Guardar imagen a escala de grises.",
             initialdir = file_path.parent,
-            initialfile = file_path.stem+"_gray"+file_path.suffix,
+            initialfile = file_path.stem+"_gray",
             defaultextension = extension,
-            # filetypes = [("Image", ["*.png", "*.jpg", "*.jpeg", "*.svg"])],
             filetypes = [(f"Imagen {extension.upper()}", f"*.{extension}")]
         )
         save_file_path = Path(save_file_path)
