45e46df0bf81e22b11b0a1c0067d2729e83db884
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-09-11 17:00:22 -0600

    Proy 01: Add functions to compare orig vs transf
    Features:
    - Se añadieron las funciones para comparar la imagen antes y después de aplicarle una transformación

---
+++ Proy01/comparison.py
@@ -0,0 +1,98 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from transformations import gain_bias_transformation, gamma_correction, histogram_equalize
+
+def gain_bias_comparison(
+        image: np.ndarray, 
+        filename: str, 
+        gain: float, 
+        bias: float
+    ) -> plt.Figure:
+    """
+    Función para comparar visualmente los 
+    efecto de aplicar un gain y bias.
+
+    Devuelve el plot diseñado.
+    """
+
+    fig, axes = plt.subplots(
+        ncols = 2,
+        subplot_kw = {
+            "yticks": [],
+            "xticks": [],
+        },
+        figsize = (10, 4.5),
+    )
+
+    fig.suptitle(f"Comparativa de Usar Gain-Bias en la Imagen {filename[-5]}")
+
+    axes[0].imshow(image)
+    axes[0].set_title("Imagen original")
+
+    axes[1].imshow(gain_bias_transformation(image, gain, bias))
+    axes[1].set_title(f"Imagen Gain-Bias")
+
+    return fig
+
+def gamma_correction_comparison(
+        image: np.ndarray, 
+        filename: str, 
+        lower_gamma: float, 
+        higher_gamma: float
+    ) -> plt.Figure:
+    """
+    Función para comparar visualmente los efectos 
+    de correcciones gammas con diferentes valores.
+
+    Devuelve el plot diseñado.
+    """
+
+    fig, axes = plt.subplots(
+        ncols = 3,
+        subplot_kw = {
+            "yticks": [],
+            "xticks": [],
+        },
+        figsize = (14, 4),
+    )
+
+    fig.suptitle(f"Comparativa de Diferentes Correcciones Gamma en la Imagen {filename[-5]}")
+
+    axes[0].imshow(gamma_correction(image, lower_gamma))
+    axes[0].set_title(f"Gamma igual a {lower_gamma:.2f}")
+
+    axes[1].imshow(image)
+    axes[1].set_title("Imagen original (Sin corrección)")
+
+    axes[2].imshow(gamma_correction(image, higher_gamma))
+    axes[2].set_title(f"Gamma igual a {higher_gamma:.2f}")
+
+    return fig
+
+def histogram_equalize_comparison(image: np.ndarray, filename: str) -> plt.Figure:
+    """
+    Función para comparar visualmente los efectos 
+    de la ecualización por histograma.
+
+    Devuelve el plot diseñado.
+    """
+
+    fig, axes = plt.subplots(
+        ncols = 2,
+        subplot_kw = {
+            "yticks": [],
+            "xticks": [],
+        },
+        figsize = (10, 4.5),
+    )
+
+    fig.suptitle(f"Comparativa de la Ecualización por Histograma en la Imagen {filename[-5]}")
+
+    axes[0].imshow(image)
+    axes[0].set_title("Imagen original")
+
+    axes[1].imshow(histogram_equalize(image))
+    axes[1].set_title(f"Imagen ecualizada")
+
+    return fig
